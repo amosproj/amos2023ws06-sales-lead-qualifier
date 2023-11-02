653af1ab01cc0b50246dcb69e2d7b18dfd3ee7ef
Felix Zailskas <felixzailskas@gmail.com>
2023-11-02 15:24:21 +0100

    Adjusted lead dummies with new data field definitions. Added field validation to Lead object
    Signed-off-by: Felix Zailskas <felixzailskas@gmail.com>

--- src/database/database_dummy.py
+++ src/database/database_dummy.py
@@ -2,6 +2,10 @@
 # SPDX-FileCopyrightText: 2023 Felix Zailskas <felixzailskas@gmail.com>
 
 import json
+from typing import List
+
+from database.models import Lead
+from database.parsers import LeadParser
 
 
 class DatabaseDummy:
@@ -10,8 +14,15 @@ class DatabaseDummy:
             json_data = json.load(f)["training_leads"]
             self.data = {d["lead_id"]: d for d in json_data}
 
-    def get_entry_by_id(self, id_: int) -> dict:
-        return self.data[id_]
+    def get_lead_by_id(self, id_: int) -> Lead:
+        return LeadParser.parse_lead_from_dict(self.data[id_])
 
-    def get_all_entries(self):
-        return self.data
+    def get_all_leads(self) -> List[Lead]:
+        leads = []
+        for entry in self.data.values():
+            leads.append(LeadParser.parse_lead_from_dict(entry))
+        return leads
+
+    def update_lead(self, lead: Lead):
+        print(f"Updating database entry for lead#{lead.lead_id}")
+        print(f"Update values: {lead}")
