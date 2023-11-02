b63905b5eea3d4238a3d3f1d3da20c9fb2fa657c
Felix Zailskas <felixzailskas@gmail.com>
2023-11-02 15:24:21 +0100

    Adjusted lead dummies with new data field definitions. Added field validation to Lead object
    Signed-off-by: Felix Zailskas <felixzailskas@gmail.com>

--- src/evp_demo.py
+++ src/evp_demo.py
@@ -4,23 +4,23 @@
 from database import get_database
 from evp.evp import EstimatedValuePredictor
 
-lead_id = 0
+lead_id = 1
 
-lead_data = get_database().get_entry_by_id(lead_id)
+lead = get_database().get_lead_by_id(lead_id)
 
 evp = EstimatedValuePredictor()
 lead_value = evp.estimate_value(lead_id)
 
 print(
     f"""
-    Dummy prediction for {lead_id=}:
+    Dummy prediction for lead#{lead.lead_id}:
 
-    Data:
-    {lead_data}
+    Lead:
+    {lead}
 
-    This lead has a predicted probability of {lead_value.customer_probability} to become a customer.
-    This lead has a predicted life time value of {lead_value.life_time_value}.
+    This lead has a predicted probability of {lead_value.customer_probability:.2f} to become a customer.
+    This lead has a predicted life time value of {lead_value.life_time_value:.2f}.
 
-    This results in a total lead value of {lead_value.get_lead_value()}.
+    This results in a total lead value of {lead_value.get_lead_value():.2f}.
 """
 )
