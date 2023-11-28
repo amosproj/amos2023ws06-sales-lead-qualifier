8f5389600cc8d2492f1891bc02b5c4500ca8da81
Fabian Utech <f.utech@gmx.net>
2023-11-28 22:39:53 +0100

    Improvement: Now can use multiple geojson files at once
    Signed-off-by: Fabian Utech <f.utech@gmx.net>

--- src/bdc/steps/regionalatlas.py
+++ src/bdc/steps/regionalatlas.py
@@ -19,9 +19,34 @@ log = get_logger()
 
 
 class RegionalAtlas(Step):
-    name: str = "Regional-Atlas"
-    reagionalatlas_feature_keys = {"gdp": "ai1701"}
-    df_fields: list[str] = reagionalatlas_feature_keys.keys()
+    name: str = "Regional_Atlas"
+    reagionalatlas_feature_keys = {
+        "pop_density":"ai0201",
+        "pop_development":"ai0202",
+        "age_0":"ai0203",
+        "age_1":"ai0204",
+        "age_2":"ai0205",
+        "age_3":"ai0206",
+        "age_4":"ai0207",
+         "pop_avg_age (Bevölkerung)":"ai0218",
+        "per_service_sector":"ai0706",        
+        "per_trade":"ai0707",
+        "employment_rate (Beschäftigt)":"ai0710",
+        "unemployment_rate (Arbeitslos)":"ai0801",
+        "per_long_term_unemployment":"ai0808",
+        "investments_p_employee":"ai1001",
+        "gross_salary_p_employee":"ai1002",
+        "disp_income_p_inhabitant":"ai1601",        
+        "tot_income_p_taxpayer":"ai1602",
+        "gdp_p_employee":"ai1701",
+        "gdp_development":"ai1702",                       
+        "gdp_p_inhabitant":"ai1703",
+        "gdp_p_workhours":"ai1704",   
+        "pop_avg_age (Gesamtbevölkerung)":"ai_z01",
+        "unemployment_rate (Erwerbslos)":"ai_z08"
+    }
+
+    df_fields: list[str] = reagionalatlas_feature_keys.values()
     #germany_gdf = osmnx.geocode_to_gdf("Germany")
 
     def __init__(self, force_refresh: bool = False) -> None:
@@ -35,49 +60,55 @@ class RegionalAtlas(Step):
         return "google_places_formatted_address" in self._df
 
     def run(self) -> DataFrame:
-        tqdm.pandas(desc="Getting info from geo data")
+        tqdm.pandas(desc="Getting social data")
         self.df[
             [f"{self.name.lower()}_{field}" for field in self.df_fields]
         ] = self.df.progress_apply(
-            lambda lead: self.get_data_from_address(lead), axis=1
-        )
+            lambda lead: pd.Series(self.get_data_from_address(lead)), axis=1        )
+        
+        self.df = self.df.rename(columns ={f"{self.name.lower()}_{v.lower()}": f"{self.name.lower()}_{k.lower()}" for k, v in self.reagionalatlas_feature_keys.items()})
         return self.df
 
     def finish(self) -> None:
-        log.info("RegionalAtlas: I am done")
+        success_rate = (1 - self.df["regional_atlas_pop_density"].isna().sum() / len(self.df["regional_atlas_pop_density"]) )*100
+        log.info("Percentage of regional information (germany): {:.2f}%".format(round(success_rate, 2)))
 
     def get_data_from_address(self, row):
+
+        empty_result = dict.fromkeys(self.reagionalatlas_feature_keys.values())
+
         if row["google_places_formatted_address"] is None:
-            return pd.Series([None])
+            return empty_result
 
         google_location = str(row["google_places_formatted_address"]).split(",")[-2:]
         google_location = [name.strip() for name in google_location]
 
-        # CHECK THE PHONENUMBER AS 2nd CRITERIUM FOR COUNTRY / CITY
+        #!!!!! CHECK THE PHONENUMBER AS 2nd CRITERIUM FOR COUNTRY / CITY
 
         country = google_location[-1].lower()
 
         # the 'regionalatlas' data is specific to germany
         if country not in ['deutschland','germany','allemagne','tyskland','germania']:
-            return pd.Series([None])
+            return empty_result
         
         # Alternative to the if 'if country not in ...'
         # if not self.germany_gdf.intersects(row_gdf):
-        #     return pd.Series([None])
+        #     return empty_result
 
         # Get the polygon of the city, to find the corresponding region
         try:
             search_gdf = osmnx.geocode_to_gdf(",".join(google_location))            
         except:
             log.info('Google location not found!')
-            return pd.Series([None])
+            return empty_result
         
         # Load the data file
+        #!!!!! INTO THE RUN PART
         try:
-            regions_gdfs = {"gdp": gpd.read_file("data/gdp.geojson")}
+            regions_gdfs = gpd.read_file("data/merged_geo.geojson") #{"gdp": gpd.read_file("data/merged_geo.geojson")}
         except:            
             log.info("File does not exist!")
-            return pd.Series([None])
+            return empty_result
                 
         # WGS 84 (used by osmnx)
         epsg_code_ll = 4326
@@ -86,45 +117,31 @@ class RegionalAtlas(Step):
         epsg_code_etrs = 25832
 
         search_gdf.crs = {"init": "epsg:{}".format(epsg_code_ll)}
-        search_gdf_reprojected = search_gdf.to_crs(epsg_code_etrs)      
+        search_gdf_reprojected = search_gdf.to_crs(epsg_code_etrs)  
+
+        # Use the centroid of the city, to check if a region 
+        search_centroid = search_gdf_reprojected.centroid       
 
         area_key = None
 
-        return_values = []
-        # For every feature we want to add ...
-        # AS A PREPROCESS STEP. PUT ALL GEOJSON TOGETHER BY LEFT JOINING THEM ON THE BASE AND THEN JUST USE THIS FILE (NO NEED TO LOOP THROUGH ALL COLUMNS)
-        # CREATE FUNCTION JOIN FOR GEOJSON (geojson,[list_geojson],key=[...,...]) [create as standard keys the ones we use]
+        return_values = {}
 
-        for col in self.df_fields:
-            regions_gdf = regions_gdfs[col]
+        # go through all regions of germany ...
+        for idx, region in regions_gdfs.iterrows():
 
-            # go through all regions of germany ...
-            for idx, region in regions_gdf.iterrows():
+            if area_key is not None:
+                if region["schluessel"] != area_key:
+                    continue
+                else:
+                    return_values.update(region[self.reagionalatlas_feature_keys.values()].to_dict())
+                    break
+            else:                                    
+                region_polygon = region["geometry"]
+                b_contains = region_polygon.contains(search_centroid).item()
 
-                if area_key is not None:
-                    if region["schluessel"] != area_key:
-                        continue
-                    else:
-                        return_values.append(
-                            region[self.reagionalatlas_feature_keys[col]]
-                        )
-                        break
-                else:                    
+                if b_contains:
+                    area_key = region["schluessel"]
+                    return_values.update(region[self.reagionalatlas_feature_keys.values()].to_dict())
+                    break
 
-                    #search_polygon = search_gdf_reprojected["geometry"].item()
-                    #b_intersect = search_polygon.intersects(region["geometry"])
-
-                    # Use the centroid of the city, to check if a region 
-                    search_centroid = search_gdf_reprojected.centroid                    
-                    region_polygon = region["geometry"]
-                    b_contains = region_polygon.contains(search_centroid).item()
-
-                    if b_contains:
-                        area_key = region["schluessel"]
-                        return_values.append(
-                            region[self.reagionalatlas_feature_keys[col]]
-                        )
-                        break
-
-
-        return pd.Series(return_values)
+        return return_values
