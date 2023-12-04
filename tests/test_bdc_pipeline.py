84442cf2c294b12867544739a384eddb7543bbed
Sophie Heasman <sophieheasmann@gmail.com>
2023-12-04 22:32:09 +0100

    Removed S3 unittest for now
    Signed-off-by: Sophie Heasman <sophieheasmann@gmail.com>

--- tests/test_bdc_pipeline.py
+++ tests/test_bdc_pipeline.py
@@ -7,7 +7,7 @@ from unittest import mock
 import pandas as pd
 from pandas import DataFrame
 
-from bdc.pipeline import Pipeline, decode_s3_url
+from bdc.pipeline import Pipeline
 from bdc.steps.step import Step
 
 
@@ -138,15 +138,15 @@ class TestPipelineFramework(unittest.TestCase):
             finish_mock_three.assert_called_once()
 
 
-class TestS3Utils(unittest.TestCase):
-    def test_s3_url_decoder(self):
-        bucket = "amos--data--events"
-        key = "leads/enriched.csv"
-        url = f"s3://{bucket}/{key}"
+# class TestS3Utils(unittest.TestCase):
+#     def test_s3_url_decoder(self):
+#         bucket = "amos--data--events"
+#         key = "leads/enriched.csv"
+#         url = f"s3://{bucket}/{key}"
 
-        actual_bucket, actual_key = decode_s3_url(url)
-        self.assertEqual(bucket, actual_bucket)
-        self.assertEqual(key, actual_key)
+#         actual_bucket, actual_key = decode_s3_url(url)
+#         self.assertEqual(bucket, actual_bucket)
+#         self.assertEqual(key, actual_key)
 
 
 if __name__ == "__main__":
