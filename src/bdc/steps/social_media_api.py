05a14ed571a4c41d46b23049a84d77a270465c39
Ahmed Sheta <ahmed.sheta@fau.de>
2023-12-12 01:36:35 +0100

    added a feature of incorperating company name in the search
    Signed-off-by: Ahmed Sheta <ahmed.sheta@fau.de>

--- src/bdc/steps/social_media_api.py
+++ src/bdc/steps/social_media_api.py
@@ -15,9 +15,56 @@ from logger import get_logger
 log = get_logger()
 
 
+def is_company_email(email):
+    commercial_domains = [
+        "web.de",
+        "mail.com",
+        "mail.de",
+        "msn.com",
+        "gmail.com",
+        "yahoo.com",
+        "hotmail.com",
+        "aol.com",
+        "hotmail.co.uk",
+        "hotmail.fr",
+        "yahoo.fr",
+        "live.com",
+        "gmx.de",
+        "outlook.com",
+        "icloud.com",
+        "outlook.de",
+        "online.de",
+        "gmx.net",
+        "googlemail.com",
+        "yahoo.de",
+        "t-online.de",
+        "gmx.ch",
+        "gmx.at",
+        "hotmail.ch",
+        "live.nl",
+        "hotmail.de",
+        "home.nl",
+        "bluewin.ch",
+        "freenet.de",
+        "upcmail.nl",
+        "zeelandnet.nl",
+        "hotmail.nl",
+        "arcor.de",
+        "aol.de",
+        "me.com",
+        "gmail.con",
+        "office.de",
+        "my.com",
+    ]
+
+    if email.split("@")[1] in commercial_domains:
+        return False
+    else:
+        return True
+
+
 class FacebookGraphAPI(Step):
     name = "Facebook_Graph"
-    added_cols = ["Full Name", "email"]
 
     def load_data(self) -> None:
         pass
@@ -28,21 +75,33 @@ class FacebookGraphAPI(Step):
         )
 
     def run(self):
-        self.df["Full Name"] = self.df["First Name"] + " " + self.df["Last Name"]
-        self.desired_fields = ["email"]
+        # choosing company name as search query if email is not in commercial domain, otherwise choose first and last names
+        self.df["Company-bool"] = self.df.apply(
+            lambda row: True if is_company_email(row["Email"]) else False, axis=1
+        )
+        self.df["search-query"] = self.df.apply(
+            lambda row: row["Email"]
+            if is_company_email(row["Email"])
+            else row["First Name"] + " " + row["Last Name"],
+            axis=1,
+        )
+        self.desired_fields = ["email"]  # category, about
         for field in self.desired_fields:
             self.df[field] = None
         tqdm.pandas(desc="Searching Facebook Graph API")
         try:
-            self.df["Full Name"].progress_apply(
+            self.df["search-query"].progress_apply(
                 lambda lead: self.search_facebook_graph(lead)
             )
         except ValueError as e:
             log.error(f"Error: {e}")
 
+        # delete the column search-query from the df at the end
+        self.df.drop("search-query", axis=1, inplace=True)
+
         return self.df
 
-    def search_facebook_graph(self, full_name):
+    def search_facebook_graph(self, search_query):
         search_results = None
         access_token = None
         user_email = None
@@ -61,7 +120,9 @@ class FacebookGraphAPI(Step):
             log.error(f"Failed to retrieve a new access token")
         graph = facebook.GraphAPI(access_token)
         try:
-            search_results = graph.request("/search", {"q": full_name, "type": "user"})
+            search_results = graph.request(
+                "/search", {"q": search_query, "type": "user"}
+            )
             if isinstance(search_results, dict) and search_results.get("data"):
                 user = search_results["data"][0]
                 user_id = user.get("id")
@@ -72,9 +133,9 @@ class FacebookGraphAPI(Step):
                     log.debug(f"User Email: {user_email}")
                     self.df["email"] = user_email
                 else:
-                    log.info(f"No user email found for {full_name}!")
+                    log.info(f"No user email found for {search_query}!")
             else:
-                log.info(f"No users found with the given name {full_name}!")
+                log.info(f"No users found with the given name {search_query}!")
         except facebook.GraphAPIError as e:
             log.error(f"Graph API Error: {e}")
 
