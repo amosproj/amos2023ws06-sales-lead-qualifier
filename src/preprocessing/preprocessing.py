062f7b22d61da12855b7309b17f9e9274b01c2e5
Ahmed Sheta <ahmed.sheta@fau.de>
2023-12-31 15:01:38 +0100

    adjusted the label encoding to hot one encoding for nominal categorical data and add scaling functionality only for the data that require scaling
    Signed-off-by: Ahmed Sheta <ahmed.sheta@fau.de>

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -10,9 +10,11 @@ import pandas as pd
 from scipy import stats
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import (
-    LabelEncoder,
     MinMaxScaler,
     MultiLabelBinarizer,
+    Normalizer,
+    OneHotEncoder,
+    RobustScaler,
     StandardScaler,
 )
 
@@ -29,11 +31,10 @@ log = get_logger()
 class Preprocessing:
     def __init__(self, df):
         self.preprocessed_df = df.copy()
-        self.added_classes = None
-
+        self.added_classes = []
         self.numerical_data = [
-            "google_places_user_ratings_total",
             "google_places_rating",
+            "google_places_user_ratings_total",
             "google_places_confidence",
             "reviews_sentiment_score",
             "review_avg_grammatical_score",
@@ -42,6 +43,8 @@ class Preprocessing:
             "review_lowest_rating_ratio",
             "review_rating_trend",
         ]
+
+        self.data_to_scale = ["google_places_user_ratings_total"]
 
         self.categorical_data = [
             "number_country",
@@ -71,6 +74,20 @@ class Preprocessing:
         )
         return self.preprocessed_df
 
+    def robust_scaling(self, column):
+        scaler = RobustScaler()
+        self.preprocessed_df[column] = scaler.fit_transform(
+            self.preprocessed_df[[column]]
+        )
+        return self.preprocessed_df
+
+    def normalization(self, column):
+        scaler = Normalizer()
+        self.preprocessed_df[column] = scaler.fit_transform(
+            self.preprocessed_df[[column]]
+        )
+        return self.preprocessed_df
+
     def remove_outliers_zscore(self, column):
         z_scores = stats.zscore(self.preprocessed_df[[column]])
         self.preprocessed_df[column] = self.preprocessed_df[
@@ -78,15 +95,17 @@ class Preprocessing:
         ]
         return self.preprocessed_df
 
-    def single_label_encoding(self, column):
-        # data is a dataframe column of the desired data for pre-processing
-        label_encoder = LabelEncoder()
-        data_encoded = label_encoder.fit_transform(
-            self.preprocessed_df[column].fillna("").astype(str)
+    def single_one_hot_encoding(self, column):
+        data_to_encode = self.preprocessed_df[[column]].fillna("").astype(str)
+        encoder = OneHotEncoder(sparse=False)
+        encoded_data = encoder.fit_transform(data_to_encode)
+        encoded_columns = encoder.get_feature_names_out([column])
+        self.added_classes.extend(encoded_columns)
+        encoded_df = pd.DataFrame(
+            encoded_data, columns=encoded_columns, index=self.preprocessed_df.index
         )
-        data_encoded_df = pd.DataFrame(data_encoded)
-        # self.preprocessed_df = pd.concat([self.preprocessed_df, data_encoded_df], axis=1)
-        self.preprocessed_df[column] = data_encoded_df
+        self.preprocessed_df = pd.concat([self.preprocessed_df, encoded_df], axis=1)
+
         return self.preprocessed_df
 
     def multiple_label_encoding(self, column):
@@ -97,38 +116,51 @@ class Preprocessing:
         )
         mlb = MultiLabelBinarizer()
         encoded_data = mlb.fit_transform(self.preprocessed_df[column])
-        self.added_classes = mlb.classes_
-        encoded_df = pd.DataFrame(encoded_data, columns=self.added_classes)
+        self.added_classes.extend(mlb.classes_)
+        encoded_df = pd.DataFrame(encoded_data, columns=mlb.classes_)
         self.preprocessed_df = pd.concat([self.preprocessed_df, encoded_df], axis=1)
         return self.preprocessed_df
 
     def implement_pipeline(self):
         for data_column in self.numerical_data:
             self.preprocessed_df = self.fill_missing_values(data_column)
-            self.preprocessed_df = self.min_max_scaling(data_column)
+            if data_column in self.data_to_scale:
+                self.preprocessed_df = self.robust_scaling(data_column)
 
         for data_column in self.categorical_data:
             if data_column == "google_places_detailed_type":
                 continue
-            self.preprocessed_df = self.single_label_encoding(data_column)
+            try:
+                self.preprocessed_df = self.single_one_hot_encoding(data_column)
+            except ValueError as e:
+                log.error(
+                    f"Failed to hot-one encode data type ({data_column})! Error: {e}"
+                )
 
-        self.preprocessed_df = self.multiple_label_encoding(
-            "google_places_detailed_type"
-        )
+        try:
+            self.preprocessed_df = self.multiple_label_encoding(
+                "google_places_detailed_type"
+            )
+        except ValueError as e:
+            log.error(
+                f"Failed to hot-one encode data type 'google_places_detailed_type'! Error: {e}"
+            )
 
         log.info("Preprocessing complete!")
         return self.preprocessed_df
 
     def save_preprocessed_data(self):
-        self.categorical_data.remove("google_places_detailed_type")
         columns_to_save = []
         columns_to_save.extend(self.numerical_data)
-        columns_to_save.extend(self.categorical_data)
         columns_to_save.extend(self.added_classes)
+
         selected_df = self.preprocessed_df[columns_to_save]
-        save_path = os.path.join(current_dir, "../data/preprocessed_data.csv")
-        selected_df.to_csv(save_path, index=False)
-        log.info(f"Preprocessed file saved at {save_path}")
+        try:
+            save_path = os.path.join(current_dir, "../data/preprocessed_data.csv")
+            selected_df.to_csv(save_path, index=False)
+            log.info(f"Preprocessed file saved at {save_path}")
+        except ValueError as e:
+            log.error(f"Failed to save preprocessed file! Error: {e}")
 
 
 if __name__ == "__main__":
