d9195678296a562380c30586763374b559924b7c
Berkay Bozkurt <resitberkaybozkurt@gmail.com>
2024-02-03 13:02:10 +0100

    demo/__init__.py failing imports are added
    Signed-off-by: Berkay Bozkurt <resitberkaybozkurt@gmail.com>

--- src/demo/__init__.py
+++ src/demo/__init__.py
@@ -1,11 +1,6 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2023 Berkay Bozkurt <resitberkaybozkurt@gmail.com>
 
-from .console_utils import get_int_input, get_multiple_choice, get_yes_no_input
-from .demos import (
-    evp_demo,
-    pipeline_demo,
-    predict_MerchantSize_on_lead_data_demo,
-    preprocessing_demo,
-)
+from .console_utils import *
+from .demos import *
 from .pipeline_utils import *
